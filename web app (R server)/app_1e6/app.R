cd888f68a2ce6a7f775421297a35c2ddb87894a1
sedufau <stephane.dufau@univ-amu.fr>
2022-10-28 21:29:07 +1000

    General update

---
+++ web app (R server)/app_1e6/app.R
@@ -0,0 +1,128 @@
+
+library(shiny)
+library(shinydashboard) # dashboard ui
+library(readr) # read tabulated separated file
+library(fs) # file operations independant from operating system
+library(DT) # data table display
+
+
+# Define UI
+ui <- dashboardPage(
+  title = "Multilex",
+  dashboardHeader(title = uiOutput("title_val")),
+  dashboardSidebar(br(),
+                   tags$h4('File reading options'),
+                   hr(),
+                   radioButtons("rbcol", label = "Columns",
+                                choices = list("Sequences & Frequency" = 2,"All" = 1),
+                                selected = 2,
+                                width = "100%",
+                                inline = FALSE),
+                   hr(),
+                   tags$h4('Interface'),
+                   br(),
+                   tags$ul(tags$li(tags$b('File reading options, columns'), ' : the initial display uses a 2-column table. Displaying "all" shows all the columns.'),
+                           tags$li(tags$b('Show button'), ' : shows up to 5000 rows on a single page.'),
+                           tags$li(tags$b('Copy button'), ' : copies what is displayed in the clipboard.'),
+                           tags$li(tags$b('CSV, XLS, PDF buttons'), ' : generates a file from a selection.'),
+                           tags$li(tags$b('Generic search box'), ' : searches entire rows for a particular term whatever its within-row position (e.g. "verb", "adj", "sun").'),
+                           tags$li(tags$b('Column headers'), ' : click on a header to sort the row selection (alaphabetical, numerical).'),
+                           tags$li(tags$b('Specific column-based filter boxes'), ' : filters rows on a particular term in a specific position (e.g. "the" in the 1st position filter box).')),
+                   tags$style(HTML("hr {border-top: 1px solid #000000;}")),
+                   tags$style(HTML("h4 {margin-left: 15px; color:#FEDA72;}")),
+                   tags$style(HTML("ul {margin-right: 5px;font-size:0.8em}"))
+
+  ),
+  dashboardBody(
+    fluidRow(column(12, DT::dataTableOutput("dt"))),
+    tags$head(tags$link(rel="shortcut icon", href="favicon.ico"))
+  )
+
+)
+
+
+
+
+
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output, session) {
+
+  observe({
+
+    # get URL parameter
+    query <- parseQueryString(session$clientData$url_search)
+
+    # set lang
+    if (!is.null(query$lang)) {
+      lang_val <- query$lang
+    } else {
+      lang_val <- "FRE"
+    }
+
+    # set n ngram
+    if (!is.null(query$n)) {
+      n_val <- query$n
+    } else {
+      n_val <- "4"
+    }
+
+
+    # set length of the dqtqset
+    #rbrow = 50000
+    #rbrow_val = "50k"
+    rbrow == 1000000
+    rbrow_val = "million"
+
+
+    # read file
+    filename <- paste0(lang_val, n_val, "_", rbrow_val, ".csv.gz")
+
+    # show the modal view
+    showModal(modalDialog(paste("Reading", filename), footer=NULL))
+
+    if (input$rbcol == "2"){
+      # read only 2 columns and assign types explicitly
+      data <- read_tsv(path("..", "data", filename), col_names = TRUE, col_types = cols_only("NGRAM" = col_character(), "FPM" = col_double()))
+    } else {
+      #read all columns and assign types implicitly (all column types will be imputed from the first 1000 rows on the input)
+      data <- read_tsv(path("..", "data", filename), col_names = TRUE, col_types = NULL)
+    }
+
+    # remove the modal view
+    removeModal()
+
+    # Set variables according to URL GET information
+    if (lang_val == "FRE"){
+      title_val = paste0("FRENCH ", n_val, "-GRAMS")
+    } else if (lang_val == "ENG"){
+      title_val = paste0("ENGLISH ", n_val, "-GRAMS")
+    } else {
+      title_val = paste0("FRENCH ", n_val, "-GRAMS")
+    }
+
+    # Set texts in UI
+    output$title_val <- renderUI({ h3(title_val) })
+
+    # shuffle grams
+    d <- data[sample(nrow(data)), , drop=FALSE]
+
+    # output data table
+    output$dt <- DT::renderDataTable({ d }, filter="top", selection="multiple", escape=FALSE,
+                                     extensions = c("Buttons"),
+                                     options = list(dom = 'Bfrtip',
+                                                    buttons = c('pageLength','copy', 'csv', 'excel', 'pdf'),
+                                                    lengthMenu = list(c(100, 1000, 10000, -1), c('100', '1000', '10000', 'all')),
+                                                    pageLength = 100,
+                                                    scrollX = TRUE)) # render table
+
+  })
+
+
+
+
+
+}
+
+shinyApp(ui, server)
+
--- web app (R server)/js/appcard_generic.js
+++ web app (R server)/js/appcard_generic.js
@@ -7,10 +7,7 @@ var file_description_fr = [
 		"description":"The Part-of-Speech-tagged 2-gram lexicon containing e.g. <b>meilleure_ADJ négociatrice_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/FRE2_general.csv.gz",
 		"filenamexlsx":"data/FRE2_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214248",
 		"applink":"apps/?lang=FRE&n=2",
-		"applink_50K":"app_50K/?lang=FRE&n=2",
-		"applink_1e6":"app_1e6/?lang=FRE&n=2",
 		"nblines":"7,548,063",
 	},
 	{
@@ -18,10 +15,7 @@ var file_description_fr = [
 		"description":"The Part-of-Speech-tagged 3-gram lexicon containing e.g. <b>la_DET messe_NOUN ésotérique_ADJ</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/FRE3_general.csv.gz",
 		"filenamexlsx":"data/FRE3_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214248",
 		"applink":"apps/?lang=FRE&n=3",
-		"applink_50K":"app_50K/?lang=FRE&n=3",
-		"applink_1e6":"app_1e6/?lang=FRE&n=3",
 		"nblines":"15,425,851",
 	},
 	{
@@ -29,10 +23,7 @@ var file_description_fr = [
 		"description":"The Part-of-Speech-tagged 4-gram lexicon containing e.g. <b>pour_ADP apaiser_VERB son_DET chagrin_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/FRE4_general.csv.gz",
 		"filenamexlsx":"data/FRE4_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214248",
 		"applink":"apps/?lang=FRE&n=4",
-		"applink_50K":"app_50K/?lang=FRE&n=4",
-		"applink_1e6":"app_1e6/?lang=FRE&n=4",
 		"nblines":"16,313,733",
 	},
 	{
@@ -40,10 +31,7 @@ var file_description_fr = [
 		"description":"The Part-of-Speech-tagged 5-gram lexicon containing e.g. <b>en_ADP moins_ADV de_ADP trois_DET ans_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/FRE5_general.csv.gz",
 		"filenamexlsx":"data/FRE5_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214248",
 		"applink":"apps/?lang=FRE&n=5",
-		"applink_50K":"app_50K/?lang=FRE&n=5",
-		"applink_1e6":"app_1e6/?lang=FRE&n=5",
 		"nblines":"12,412,134"
 	}
 ];
@@ -54,10 +42,7 @@ var file_description_en = [
 		"description":"The Part-of-Speech-tagged 2-gram lexicon containing e.g. <b>same_ADJ sentiment_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/ENG2_general.csv.gz",
 		"filenamexlsx":"data/ENG2_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214223",
 		"applink":"apps/?lang=ENG&n=2",
-		"applink_50K":"app_50K/?lang=ENG&n=2",
-		"applink_1e6":"app_1e6/?lang=ENG&n=2",
 		"nblines":"8,161,430",
 	},
 	{
@@ -65,10 +50,7 @@ var file_description_en = [
 		"description":"The Part-of-Speech-tagged 3-gram lexicon containing e.g. <b>colours_NOUN were_VERB different_ADJ</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/ENG3_general.csv.gz",
 		"filenamexlsx":"data/ENG3_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214223",
 		"applink":"apps/?lang=ENG&n=3",
-		"applink_50K":"app_50K/?lang=ENG&n=3",
-		"applink_1e6":"app_1e6/?lang=ENG&n=3",
 		"nblines":"15,448,922",
 	},
 	{
@@ -76,10 +58,7 @@ var file_description_en = [
 		"description":"The Part-of-Speech-tagged 4-gram lexicon containing e.g. <b>the_DET prescribed_VERB time_NOUN limit_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/ENG4_general.csv.gz",
 		"filenamexlsx":"data/ENG4_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214223",
 		"applink":"apps/?lang=ENG&n=4",
-		"applink_50K":"app_50K/?lang=ENG&n=4",
-		"applink_1e6":"app_1e6/?lang=ENG&n=4",
 		"nblines":"15,116,507",
 	},
 	{
@@ -87,10 +66,7 @@ var file_description_en = [
 		"description":"The Part-of-Speech-tagged 5-gram lexicon containing e.g. <b>the_DET life_NOUN and_CONJ death_NOUN struggle_NOUN</b> with its annual frequency averaged over the period 2012-2019",
 		"filename":"data/ENG5_general.csv.gz",
 		"filenamexlsx":"data/ENG5_million.xlsx",
-		"zenodofile":"https://zenodo.org/record/7214223",
 		"applink":"apps/?lang=ENG&n=5",
-		"applink_50K":"app_50K/?lang=ENG&n=5",
-		"applink_1e6":"app_1e6/?lang=ENG&n=5",
 		"nblines":"10,602,231"
 	}
 ];
@@ -119,13 +95,13 @@ for (i=0; i<file_description_fr.length; i++){
 	"      <br><br> " +
 	"      <div class='btn-toolbar float-right' role='toolbar' aria-label='Toolbar with button groups'>" +
 	"        <div class='btn-group mr-2' role='group' aria-label='First group'>" +
-	"          <a href='"+ file_description_fr[i].zenodofile +"' class='btn btn-secondary'>Download files<br>French sequences</a> " +
+	"          <a href='"+ file_description_fr[i].filename +"' class='btn btn-secondary'>Download csv file<br>(all sequences, gzipped)</a> " +
+	"        </div>" +
+	"        <div class='btn-group mr-2' role='group' aria-label='First group'>" +
+	"          <a href='"+ file_description_fr[i].filenamexlsx +"' class='btn btn-secondary'>Download xlsx file<br>(a million sequences)</a> " +
 	"        </div>" +
 	"        <div class='btn-group mr-2' role='group' aria-label='Second group'>" +
-	"          <a href='"+ file_description_fr[i].applink_50K +"' class='btn btn-info'>Launch app<br>(50K sequences)</a> " +
-	"        </div>" +
-	"        <div class='btn-group mr-2' role='group' aria-label='Second group'>" +
-	"          <a href='"+ file_description_fr[i].applink_1e6 +"' class='btn btn-info'>Launch app<br>(1 million sequences)</a> " +
+	"          <a href='"+ file_description_fr[i].applink +"' class='btn btn-info'>Launch<br>app</a> " +
 	"        </div>" +
 	"      </div>" +
 	"    </div> " +
@@ -148,13 +124,13 @@ for (i=0; i<file_description_en.length; i++){
 	"      <br><br> " +
 	"      <div class='btn-toolbar float-right' role='toolbar' aria-label='Toolbar with button groups'>" +
 	"        <div class='btn-group mr-2' role='group' aria-label='First group'>" +
-	"          <a href='"+ file_description_en[i].zenodofile +"' class='btn btn-secondary'>Download files<br>English sequences</a> " +
+	"          <a href='"+ file_description_en[i].filename +"' class='btn btn-secondary'>Download csv files<br>(all sequences, gzipped)</a> " +
+	"        </div>" +
+	"        <div class='btn-group mr-2' role='group' aria-label='First group'>" +
+	"          <a href='"+ file_description_en[i].filenamexlsx +"' class='btn btn-secondary'>Download xlsx file<br>(a million sequences)</a> " +
 	"        </div>" +
 	"        <div class='btn-group mr-2' role='group' aria-label='Second group'>" +
-	"          <a href='"+ file_description_en[i].applink_50K +"' class='btn btn-info'>Launch app<br>(50K sequences)</a> " +
-	"        </div>" +
-	"        <div class='btn-group mr-2' role='group' aria-label='Second group'>" +
-	"          <a href='"+ file_description_en[i].applink_1e6 +"' class='btn btn-info'>Launch app<br>(1 million sequences)</a> " +
+	"          <a href='"+ file_description_en[i].applink +"' class='btn btn-info'>Launch<br>app</a> " +
 	"        </div>" +
 	"      </div>" +
 	"    </div> " +
